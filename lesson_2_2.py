3aeeadf3149554b69a926182b7b78a216ab7e865
perekrestok-integritas <sorsha999@gmail.com>
2026-08-17 20:28:42 +0400

    update lesson1, lesson2

---
+++ lesson_2_2.py
@@ -0,0 +1,18 @@
+s1 = "Sandra"
+s2 = 'Integritas'
+print(s1, s2)
+s3 = "Sandra\nIntegritas"
+s4 = "Sandra\n Integritas"
+print(s3)
+print(s4)
+s5 = """
+1jlkjlj
+
+hjkhk
+
+hgygygy
+jjjjjjjjjj
+"""
+print(s5)
+
+# многострочные кавычки переносят по строкам именно так, как записано
